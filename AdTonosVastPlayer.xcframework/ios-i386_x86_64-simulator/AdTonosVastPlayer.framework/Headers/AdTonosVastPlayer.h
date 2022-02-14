8c6de0c85ff1a4b981c9e33b6f2a30579fe44d58
Robert Kawecki <robert+public@rkaw.pl>
2022-02-14 14:37:04 +0100

    Import SDK files

---
+++ AdTonosVastPlayer.xcframework/ios-i386_x86_64-simulator/AdTonosVastPlayer.framework/Headers/AdTonosVastPlayer.h
@@ -0,0 +1,18 @@
+//
+//  AdTonosVastPlayer.h
+//  AdTonosVastPlayer
+//
+//  Created by Mateusz Wojnar on 13/10/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdTonosVastPlayer.
+FOUNDATION_EXPORT double AdTonosVastPlayerVersionNumber;
+
+//! Project version string for AdTonosVastPlayer.
+FOUNDATION_EXPORT const unsigned char AdTonosVastPlayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdTonosVastPlayer/PublicHeader.h>
+
+
